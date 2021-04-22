2b49928aaadb58179100bd378cd0148eda82b933
RobertJia <sjia0385@uni.sydney.edu.au>
2021-04-22 13:05:28 +1000

    shell update

---
+++ browse_phantomjs.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+#while true; do
+        d=`date "+%d-%m-%y-%H%M%S"`
+        #mkdir ./pcaps/
+        mkdir ./pcaps/phantomjs/$d
+        #sudo systemctl stop cloudflared
+        #sleep 3
+        sudo ln -sf /home/ubuntu/doh_traffic_analysis/code/collection/resolv.conf /etc/resolv.conf
+        for i in $(seq 201 1486)
+        do
+                echo $i
+                sudo /usr/sbin/tcpdump -i any "port 53 || host 1.1.1.1" -w ./pcaps/phantomjs/$d/phantomjs$i.pcap &
+                sleep 2
+                python3 ./phantomjs_driver.py $i
+                sleep 2
+                sudo pkill tcpdump
+                sleep 2
+                sudo systemd-resolve --flush-caches
+                sleep 2
+        done
+        #sleep 300
+        #sudo systemctl start cloudflared
+#done
