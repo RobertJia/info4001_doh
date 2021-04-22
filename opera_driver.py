e101c9f04d751847d57b777f05d433e61c80d02b
RobertJia <sjia0385@uni.sydney.edu.au>
2021-04-22 13:03:02 +1000

    browser driver update

---
+++ opera_driver.py
@@ -0,0 +1,43 @@
+import time
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+import subprocess
+from pyvirtualdisplay import Display
+import sys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+start = time.time()
+INTERVAL_TIME = 1 #Interval time between queries
+urls = []
+ct = 0
+
+fname = "./short_list_1500"
+with open(fname) as f:
+	lines = f.readlines()
+	for line in lines:
+		urls.append(line.strip())
+
+url = urls[int(sys.argv[1])]
+print(url)
+#display = Display(visible=0, size=(800, 800))
+#display.start()
+#print("Started display")
+#options = webdriver.ChromeOptions()
+
+#options.add_argument('--headless')
+#options.add_argument('--no-sandbox')
+driver = webdriver.Opera(executable_path='/snap/bin/opera')
+#driver = webdriver.Chrome('./chromedriver', options=options)
+driver.set_page_load_timeout(30)
+print("Started driver")
+url = 'http://' + url
+try:
+	driver.get(url)
+	time.sleep(INTERVAL_TIME*5)
+except TimeoutException as ex:
+	print(ex)
+driver.quit()
+#display.stop()
+stop = time.time()
+print("Time taken:" + str(stop - start))
+time.sleep(INTERVAL_TIME)
