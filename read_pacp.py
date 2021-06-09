c7ed6982ec8938e5b9d794241ca2bb281db2f1fc
RobertJia <sjia0385@uni.sydney.edu.au>
2021-06-10 08:13:00 +1000

    test one site

--- read_pacp.py
+++ read_pacp.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import collections
 import numpy as np
 
-
 def get_feature_vec(filename):
 	try: 
 		traffic = rdpcap(filename)
@@ -12,10 +11,10 @@ def get_feature_vec(filename):
 		data = []
 		for record in traffic:
 			if str(record['IP'].src)=='1.1.1.1' or str(record['IP'].dst)=='1.1.1.1' :
-			    if str(record['IP'].dst)=='1.1.1.1' and (len(record['TCP'].payload)>0):
-			        data.append(len(record['TCP'].payload))
-			    elif (len(record['TCP'].payload)>0):
-			        data.append(-len(record['TCP'].payload))
+				if str(record['IP'].dst)=='1.1.1.1' and (len(record['TCP'].payload)>0):
+					data.append(len(record['TCP'].payload))
+				elif len(record['TCP'].payload)>0:
+					data.append(-len(record['TCP'].payload))
 
 		return np.array(data)
 
@@ -30,37 +29,67 @@ def get_feature_vec(filename):
 
 ############# globals
 LENGTH = 202
+chrome_avg_in = []
+chrome_avg_out = []
+firefox_avg_in = []
+firefox_avg_out = []
+phantomjs_avg_in = []
+phantomjs_avg_out = []
+
 chrome_avgs = []
 firefox_avgs = []
 phantomjs_avgs = []
 #############
 
 # chrome
-for i in range(LENGTH){
-	res = get_feature_vec('pcaps/chrome/test_large_scale/pcaps/chrome{}.pcap'.format(i));
+for i in range(LENGTH):
+	res = get_feature_vec('pcaps/chrome/test_large_scale/chrome{}.pcap'.format(i))
 	chrome_avgs.append(np.average(res))
-}
+
 
 # firefox
-for i in range(LENGTH){
-	res = get_feature_vec('pcaps/firefox/test_large_scale/pcaps/firefox{}.pcap'.format(i));
+for i in range(LENGTH):
+	res = get_feature_vec('pcaps/firefox/test_large_scale/firefox{}.pcap'.format(i));
 	firefox_avgs.append(np.average(res))
-}
 
 # phantomjs
-for i in range(LENGTH){
-	res = get_feature_vec('pcaps/phantomjs/test_large_scale/pcaps/phantomjs{}.pcap'.format(i));
+for i in range(LENGTH):
+	res = get_feature_vec('pcaps/phantomjs/test_large_scale/phantomjs{}.pcap'.format(i))
 	phantomjs_avgs.append(np.average(res))
-}
+
 
 # list to array
-chrome_avgs = np.array(chrome_avgs)
-firefox_avgs = np.array(firefox_avgs)
-phantomjs_avgs = np.array(phantomjs_avgs)
+chrome_avgs_new = np.array(chrome_avgs)
+firefox_avgs_new = np.array(firefox_avgs)
+phantomjs_avgs_new = np.array(phantomjs_avgs)
+
+# remove NaN
+# chrome_nan_array = np.isnan(chrome_avgs)
+# chrome_not_nan_array = ~ chrome_nan_array
+# chrome_avgs_new = chrome_avgs[chrome_not_nan_array]
+
+# firefox_nan_array = np.isnan(firefox_avgs)
+# firefox_not_nan_array = ~ firefox_nan_array
+# firefox_avgs_new = firefox_avgs[firefox_not_nan_array]
+
+# phantomjs_nan_array = np.isnan(phantomjs_avgs)
+# phantomjs_not_nan_array = ~ phantomjs_nan_array
+# phantomjs_avgs_new = phantomjs_avgs[phantomjs_not_nan_array]
+
+print(chrome_avgs_new)
+print(firefox_avgs_new)
+print(phantomjs_avgs_new)
 
 # plots
-plt.boxplot(chrome_avgs)
-
+fig, axs = plt.subplots(3)
+fig.suptitle('average packet lengths of chrome, firefox and phantomjs')
+axs[0].set_title('chrome')
+axs[0].plot(chrome_avgs_new, 'tab:orange')
+axs[1].set_title('firefox')
+axs[1].plot(firefox_avgs_new, 'tab:green')
+axs[2].set_title('phantomjs')
+axs[2].plot(phantomjs_avgs_new, 'tab:blue')
+plt.show()
 # res = get_feature_vec(filename)
 # print(res)
 # plt.plot(res)
---
+++ test_one_site/browse_firefox.sh
@@ -0,0 +1,25 @@
+#/usr/bin/bash
+#while true; do
+        #d=`date "+%d-%m-%y-%H%M%S"`
+        #mkdir ./pcaps/
+        mkdir ../pcaps/chrome/test_one_site
+        #sudo systemctl stop cloudflared
+        #sleep 3
+        #sudo ln -sf /home/ubuntu/doh_traffic_analysis/code/collection/resolv.conf /etc/resolv.conf
+
+        # hit the target webiste 20 times
+        for i in $(seq 0 19)
+        do
+                echo $i
+                sudo /usr/sbin/tcpdump -i any "port 53 || host 1.1.1.1" -w ../pcaps/firefox/test_one_site/firefox$i.pcap &
+                sleep 2
+                python3 ./firefox_driver.py $i
+                sleep 2
+                sudo pkill tcpdump
+                sleep 2
+                sudo systemd-resolve --flush-caches
+                sleep 2
+        done
+        #sleep 300
+        #sudo systemctl start cloudflared
+#done
